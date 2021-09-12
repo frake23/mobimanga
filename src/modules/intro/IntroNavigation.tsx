548607e8b840a7cc11a58be3d6ec492e68b6b56b
stufade <stufade223@gmail.com>
2021-09-13 00:30:20 +0300

    First animation

--- src/modules/intro/IntroNavigation.tsx
+++ src/modules/intro/IntroNavigation.tsx
@@ -1,42 +1,51 @@
 import React from 'react';
-import { useWindowDimensions } from 'react-native';
-import { StyleSheet, View } from 'react-native';
-import { TouchableOpacity } from 'react-native';
-import { SvgProps } from 'react-native-svg';
+import {Animated, Easing, useWindowDimensions} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import {SvgProps} from 'react-native-svg';
 import ArrowRightIcon from '../../icons/ArrowRight';
 import Close1Icon from '../../icons/Close1';
-import { colors } from '../../theme/colors';
+import {colors} from '../../theme/colors';
 import spacings from '../../theme/spacings';
 
 interface IntroNavigationProps {
-    viewsCount: number,
-    selected: number,
-    onNext: () => void,
-    onExit: () => void
+    viewsCount: number;
+    selected: number;
+    onNext: () => void;
+    onExit: () => void;
 }
 
 const Circle: React.FC<{size: number}> = ({size}) => {
     return (
-        <View style={[styles.circle, {height: size, width: size, borderRadius: size / 2}]}/>
-    )
-}
+        <View
+            style={[
+                styles.circle,
+                {height: size, width: size, borderRadius: size / 2},
+            ]}
+        />
+    );
+};
 
-export const IntroNavigation: React.FC<IntroNavigationProps> = ({viewsCount, selected, onExit, onNext}) => {
+export const IntroNavigation: React.FC<IntroNavigationProps> = ({
+    viewsCount,
+    selected,
+    onExit,
+    onNext,
+}) => {
     const {width} = useWindowDimensions();
     const isLast = viewsCount === selected + 1;
-    const left = []; const right = [];
-    
+    const left = [];
+    const right = [];
+
     let leftMargin = width / 2 - 30;
     for (let i = 0; i < selected; i++) {
         if (i === 0) {
             left.push(8);
             leftMargin -= 8;
-        }
-        else if (i === viewsCount - 2) {
+        } else if (i === viewsCount - 2) {
             left.push(12);
             leftMargin -= 12;
-        }
-        else {
+        } else {
             left.unshift(6);
             leftMargin -= 6;
         }
@@ -45,49 +54,66 @@ export const IntroNavigation: React.FC<IntroNavigationProps> = ({viewsCount, sel
     for (let i = selected + 1; i < viewsCount; i++) {
         if (i === selected + 1) right.push(12);
         else if (i === selected + 2) right.push(8);
-        else right.push(6)
+        else right.push(6);
     }
-    
+
+    const toBig = new Animated.Value(0);
+
+    Animated.timing(toBig, {
+        toValue: 1,
+        easing: Easing.ease,
+        duration: 250,
+        useNativeDriver: true,
+    }).start();
+
+    const animatedStyles = [
+        styles.buttton,
+        {
+            opacity: toBig,
+            transform: [
+                {
+                    scale: toBig.interpolate({
+                        inputRange: [0, 1],
+                        outputRange: [0, 6],
+                    }),
+                },
+            ],
+            top: 8,
+            marginHorizontal: 24
+        },
+    ];
+
     return (
         <View style={[styles.container, {left: leftMargin}]}>
-            {
-                left.map((size, index) => 
-                    <Circle size={size} key={index}/>    
-                )
-            }
-            <TouchableOpacity
+            {left.map((size, index) => (
+                <Circle size={size} key={index} />
+            ))}
+            <TouchableWithoutFeedback
                 onPress={isLast ? onExit : onNext}
-                style={styles.buttton}
-            >
-                {isLast ?
-                    <Close1Icon 
-                        {...iconStyles}
-                    />
-                    :
-                    <ArrowRightIcon
-                        {...iconStyles}
-                    />
-                }
-            </TouchableOpacity>
-            {
-                right.map((size, index) => 
-                    <Circle size={size} key={index}/>    
-                )
-            }
+                style={{paddingVertical: 18}}>
+                <Animated.View style={animatedStyles} />
+                {isLast ? (
+                        <Close1Icon {...iconStyles} style={styles.iconMove} />
+                    ) : (
+                        <ArrowRightIcon {...iconStyles} style={styles.iconMove} />
+                    )}
+            </TouchableWithoutFeedback>
+            {right.map((size, index) => (
+                <Circle size={size} key={index} />
+            ))}
         </View>
-        
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     buttton: {
-        width: 48,
-        height: 48,
+        width: 8,
+        height: 8,
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 24,
         backgroundColor: colors.main.primary,
-        marginHorizontal: 6
+        marginHorizontal: 6,
     },
     container: {
         flexDirection: 'row',
@@ -97,12 +123,17 @@ const styles = StyleSheet.create({
     },
     circle: {
         backgroundColor: colors.bright.fivefold,
-        marginHorizontal: 6
+        marginHorizontal: 6,
+    },
+    iconMove: {
+        left: 18,
+        position: "relative",
+        bottom: 4
     }
-})
+});
 
 const iconStyles: SvgProps = {
     height: 20,
     width: 20,
-    stroke: colors.bright.primary
-}
+    stroke: colors.bright.primary,
+};
