cf6172b9cf94a2870717cc41322271a0880c591c
stufade <stufade223@gmail.com>
2021-11-14 19:40:03 +0300

    bugs fixed

--- src/modules/manga/MangaScreen.tsx
+++ src/modules/manga/MangaScreen.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
     Image,
-    ImageBackground,
     ScrollView,
     StyleSheet,
     Text,
@@ -58,19 +57,21 @@ export const MangaScreen = () => {
     );
 
     const chapters = [
-        "1.0 Название главы",
-        "2.0 Название главы",
-        "3.0 Название главы",
-        "4.0 Название главы",
-        "5.0 Название главы",
-        "6.0 Название главы",
-    ]
+        '1.0 Название главы',
+        '2.0 Название главы',
+        '3.0 Название главы',
+        '4.0 Название главы',
+        '5.0 Название главы',
+        '6.0 Название главы',
+    ];
 
     const chapterContentToRender = (
         <View>
-            {chapters.map(chapter => <ChapterView title={chapter} key={chapter} />)}
+            {chapters.map(chapter => (
+                <ChapterView title={chapter} key={chapter} />
+            ))}
         </View>
-    )
+    );
 
     return (
         <View style={styles.container}>
@@ -80,46 +81,49 @@ export const MangaScreen = () => {
                 resizeMode="stretch"
             />
             <View style={styles.main}>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View style={styles.header}>
-                    <Text
-                        numberOfLines={2}
-                        lineBreakMode="tail"
-                        style={[textStyles.h3, styles.text]}>
-                        Манга с длинным названием
-                    </Text>
-                    <FavouriteButton type="big" isFavourite={false} />
-                </View>
-                <View style={styles.description}>
-                    <Text style={textStyles.body}>
-                        Полное описание содержания манги
-                    </Text>
-                </View>
-                <View style={styles.panelsWrapper}>
-                    <DropoutPanel
-                        title="Информация"
-                        render={infoContentToRender}
-                    />
-                    <DropoutPanel
-                        title="Главы"
-                        render={chapterContentToRender}
-                    />
-                </View>
-                <View style={styles.buttonsWrapper}>
-                    <Button
-                        title="Скачать все"
-                        color="secondary"
-                        style={styles.button}
-                        onPress={() => null}
-                    />
-                    <Button
-                        title="Читать сейчас"
-                        color="primary"
-                        style={[styles.button, styles.secondButtonMarginLeft]}
-                        onPress={() => null}
-                    />
-                </View>
-            </ScrollView>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <View style={styles.header}>
+                        <Text
+                            numberOfLines={2}
+                            lineBreakMode="tail"
+                            style={[textStyles.h3, styles.text]}>
+                            Манга с длинным названием
+                        </Text>
+                        <FavouriteButton type="big" isFavourite={false} />
+                    </View>
+                    <View style={styles.description}>
+                        <Text style={textStyles.body}>
+                            Полное описание содержания манги
+                        </Text>
+                    </View>
+                    <View style={styles.panelsWrapper}>
+                        <DropoutPanel
+                            title="Информация"
+                            render={infoContentToRender}
+                        />
+                        <DropoutPanel
+                            title="Главы"
+                            render={chapterContentToRender}
+                        />
+                    </View>
+                    <View style={styles.buttonsWrapper}>
+                        <Button
+                            title="Скачать все"
+                            color="secondary"
+                            style={styles.button}
+                            onPress={() => null}
+                        />
+                        <Button
+                            title="Читать сейчас"
+                            color="primary"
+                            style={[
+                                styles.button,
+                                styles.secondButtonMarginLeft,
+                            ]}
+                            onPress={() => null}
+                        />
+                    </View>
+                </ScrollView>
             </View>
         </View>
     );
@@ -128,21 +132,19 @@ export const MangaScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "flex-end"
+        justifyContent: 'flex-end',
     },
     image: {
-        position: "absolute",
+        position: 'absolute',
         top: 0,
         left: 0,
-        width: "100%",
-        height: "70%"
+        width: '100%',
+        height: '70%',
     },
     main: {
         paddingHorizontal: spacings.xs,
-        paddingTop: spacings.xs,
         backgroundColor: colors.bright.primary,
         zIndex: 2,
-        maxHeight: "90%"
     },
     text: {
         maxWidth: '80%',
@@ -150,14 +152,15 @@ const styles = StyleSheet.create({
     header: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: "flex-start"
+        alignItems: 'flex-start',
+        marginTop: spacings.xs,
     },
     description: {
         marginVertical: spacings.sm,
     },
     buttonsWrapper: {
         flexDirection: 'row',
-        marginBottom: spacings.xs
+        marginBottom: spacings.xs,
     },
     button: {
         flex: 1,
